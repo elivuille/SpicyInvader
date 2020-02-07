b9fe98f654755306536153510071b0d4f4a84b73
Elias <elias.vuille@eduvaud.ch>
2020-02-07 15:01:34 +0100

    la base frr

---
+++ InvasionDeLespace/Enemies.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvasionDeLespace
+{
+    class Enemies
+    {
+        public void Maingame()
+        {
+            int tics = 0;
+            int x = 3;
+            int y = 1;
+            char[,] aliens = new char[3, 6];
+
+           
+                for (int i = 0; i < 3; i++)
+                {
+                   
+                    Console.SetCursorPosition(x, y);
+                    for (int j = 0; j < 6; j++)
+                    {
+                        aliens[i, j] = 'A';
+                        Debug.WriteLine("Column : " + i + " Row : " + j + " Data " + aliens[i, j]);
+                        x += 2;
+                    Console.SetCursorPosition(x, y);
+                    Console.Write(aliens[i, j]);
+                       
+
+                    }
+                x = 3;
+                    y+=3;
+                }
+               
+            
+
+                
+                
+            
+            
+
+            
+
+           
+            
+
+
+        }
+
+        
+    }
+}
---
+++ InvasionDeLespace/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvasionDeLespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menuPrincipal = new Menu();
+            menuPrincipal.Main();
+            
+        }
+    }
+}
