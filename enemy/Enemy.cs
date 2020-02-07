04b7ea74c5a1f10b458f8cd6b4a3134b4ae1ff09
jeremie_bsy <jeremie.bussy@gmail.com>
2020-02-07 15:39:26 +0100

    les ennemis qui bougent

---
+++ enemy/Enemy.cs
@@ -0,0 +1,94 @@
+﻿/* ETML
+ * Auteur: Jérémie Bussy
+ * Date: 07.02.2020
+ * Description: classe des invaders
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enemy
+{
+    class Enemy
+    {
+        /// <summary>
+        /// Attributs
+        /// </summary>
+        private int _skyWidth; //Où sera stockée la largeur de la console
+        private int _intX = 5; //Position X, initiaisée à 5 pour pas que les ennemis touchent le bord de la console
+        private int _intY; //Position Y
+        private int _ship; //Bateau
+
+
+        /// <summary>
+        /// Propriétés
+        /// </summary>
+        public int SkyWidth
+        {
+            get { return _skyWidth; }
+            set { _skyWidth = value; }
+        }
+
+        public int IntX
+        {
+            get { return _intX; }
+            set { _intX = value; }
+        }
+
+        public int IntY
+        {
+            get { return _intY; }
+            set { _intY = value; }
+        }
+
+        public int Ship
+        {
+            get { return _ship; }
+            set { _ship = value; }
+        }
+
+
+        /// <summary>
+        /// Méthode qui gère le déplacement des ennemis
+        /// </summary>
+        public void Move()
+        {
+            _skyWidth = Console.WindowWidth-5; //-5 pour pas que les ennemis touchent le bord de la console (WindowWidth donne la largeur de la console)
+
+            Erase(); //Efface l'ancien ennemi
+
+            if (_intX < _skyWidth)
+            {
+                _intX++;
+            }
+
+            if (_intX == _skyWidth)
+            {
+                _intY += 1;
+                _intX = 5;
+            }
+
+            Draw(); //Dessine le nouvel ennemi après calcul de la nouvelle position
+        }
+
+        /// <summary>
+        /// Méthode qui dessine les ennemis
+        /// </summary>
+        public void Draw()
+        {
+            Console.SetCursorPosition(_intX, _intY);
+            Console.Write("*");
+        }
+
+        /// <summary>
+        /// Màthode qui efface les ennemis
+        /// </summary>
+        public void Erase()
+        {
+            Console.SetCursorPosition(_intX, _intY);
+            Console.Write(" ");
+        }
+    }
+}
---
+++ enemy/Program.cs
@@ -0,0 +1,28 @@
+﻿/* ETML
+ * Auteur: Jérémie Bussy
+ * Date: 07.02.2020
+ * Description: programme qui affiche l'ennemi, le fait se déplacer
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemy n = new Enemy(); //Création d'un ennemi
+
+            while (true) //Boucle qui va s'executer à l'infini
+            {
+                Console.CursorVisible = false; //Masque le curseur
+                n.Move(); //Appel de la méthode Move
+                System.Threading.Thread.Sleep(500); //S'occupe de séquencer le décalage de l'ennemi
+            }
+        }
+    }
+}
